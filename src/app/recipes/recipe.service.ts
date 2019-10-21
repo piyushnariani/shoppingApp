a6802c59b9d6cca72b4c2aa6c7fc9df18090536a
Piyush <piyushnariani09@gmail.com>
2019-10-20 23:42:05 -0400

    Added ingredients to recipe and functionality to push ingredients to shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,22 +1,34 @@
-// import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 
-// @Injectable({
-//   providedIn: 'root'
-// })
+@Injectable()
 export class RecipeService {
 
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe("testName", "testDesc", "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/600px-Recipe_logo.jpeg")
+    new Recipe(
+      "This is a test Recipe 1", 
+      "This is test description 1", 
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/600px-Recipe_logo.jpeg",
+      [
+        new Ingredient('Ingredient 1', 5),
+        new Ingredient('Ingredient 2', 10)
+      ]
+    )
   ];
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -24,4 +24,10 @@ export class ShoppingListService {
     this.ingredients.push(ingredient);
     this.ingredientsChanged.emit(this.ingredients.slice());
   }
+
+  addIngredients(ingredients: Ingredient[]){
+    // ingredients.forEach(ingredient => this.addIngredient(ingredient);
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
